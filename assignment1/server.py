4010fd8d0a3d9d258ba54711cd5f616879140ad6
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-09-30 17:30:29 +0200

    asignment1 finished

---
+++ assignment1/server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/<path:path>')
+def home(path):
+    output="/"
+    output+=path+"<br/>"
+
+    args=[k+": "+request.args[k] for k,v in request.args.items()]
+    output+="<br/>".join(args)
+
+    env=[k+"="+os.environ[k] for k,v in os.environ.items()]
+    output+="<br/><br/>Env:<br/>"+"<br/>".join(env)
+
+    return output
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=False,host='0.0.0.0')
