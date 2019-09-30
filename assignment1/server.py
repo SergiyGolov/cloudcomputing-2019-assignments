c642cd6e9f0356a8ace6aae6d27c9b6203803aa8
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-09-30 17:31:11 +0200

    fixed format of python file

--- assignment1/server.py
+++ assignment1/server.py
@@ -9,17 +9,17 @@ app = Flask(__name__)
 
 @app.route('/<path:path>')
 def home(path):
-    output="/"
-    output+=path+"<br/>"
+    output = "/"
+    output += path+"<br/>"
 
-    args=[k+": "+request.args[k] for k,v in request.args.items()]
-    output+="<br/>".join(args)
+    args = [k+": "+request.args[k] for k, v in request.args.items()]
+    output += "<br/>".join(args)
 
-    env=[k+"="+os.environ[k] for k,v in os.environ.items()]
-    output+="<br/><br/>Env:<br/>"+"<br/>".join(env)
+    env = [k+"="+os.environ[k] for k, v in os.environ.items()]
+    output += "<br/><br/>Env:<br/>"+"<br/>".join(env)
 
     return output
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=False,host='0.0.0.0')
+    app.run(port=5000, debug=False, host='0.0.0.0')
