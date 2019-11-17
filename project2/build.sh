ce9ccf87ed53b7456c234070f4355bb717656ad2
Naralas <ludovic.herbelin@gmail.com>
2019-11-17 12:12:50 +0100

    Build and push the infos services image on dockerhub

--- project2/build.sh
+++ project2/build.sh
@@ -1,1 +1,6 @@
-#/bin/bash
+#!/usr/bin/env bash
+# build and push the infos service image
+docker build -t info-service-v1 ./infos/python_server/
+
+# build and push the infos service image
+# docker build -t image-service-v1 ./images/python_server/
--- project2/deploy.sh
+++ project2/deploy.sh
@@ -1,1 +1,9 @@
-#/bin/bash
+#!/usr/bin/env bash
+# Push the infos service image on dockerhub
+docker tag info-service-v1:latest ludovicherbelin/info-service-v1:latest
+docker push ludovicherbelin/info-service-v1
+
+
+# Push the infos service image
+# docker tag image-service-v1:latest ludovicherbelin/image-service-v1:latest
+# docker push ludovicherbelin/image-service-v1
