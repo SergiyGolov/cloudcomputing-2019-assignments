f9a1c513d9d6329ff41d34ecab6cfb5c0a0ec906
Naralas <ludovic.herbelin@gmail.com>
2019-11-17 12:33:07 +0100

    Added line to deploy the kubernetes

--- project2/deploy.sh
+++ project2/deploy.sh
@@ -7,3 +7,6 @@ docker push ludovicherbelin/info-service-v1
 # Push the infos service image
 docker tag image-service-v1:latest ludovicherbelin/image-service-v1:latest
 docker push ludovicherbelin/image-service-v1
+
+# Deploy using the yaml file
+kubectl create -f all.yaml create using a yaml config file
