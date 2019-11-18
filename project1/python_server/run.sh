109fe7f4b64d2630eb3c07c484bdfb32e6d85a8c
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-11-18 11:29:32 +0100

    restored deleted files from project1

---
+++ project1/python_server/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export FLASK_APP=server.py
+export DB_HOST=127.0.0.1
+export DB_PORT=3306
+export DB_DBNAME=watches
+export DB_USER=watches
+export DB_PASS=watches
+export HTTP_USER=cloud
+export HTTP_PASS=computing
+
+flask run --port=1080
