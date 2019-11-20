7eb45a1226d9e1edda181fa45e22c412cbe7548a
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-11-20 11:11:04 +0100

    fixed info server.py to support concurrent mysql connections + updated yaml to specify a fixed number of maxUnavailable instead of a percentage

--- project2/info/server.py
+++ project2/info/server.py
@@ -18,11 +18,11 @@ http_pass = os.environ['HTTP_PASS']
 
 api_info_v1_prefix = '/info/v1'
 
-con = pymysql.connect(db_host, db_user, db_pass, db_dbname)
-
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+def create_connection():
+    return pymysql.connect(db_host, db_user, db_pass, db_dbname)
 
 @auth.verify_password
 def verify_password(username, password):
@@ -38,24 +38,28 @@ def home():
 @app.route(f'{api_info_v1_prefix}/watch/<sku>', methods=['GET'])
 @auth.login_required
 def get_watch_data(sku):
-    with con:
-        cur = con.cursor(pymysql.cursors.DictCursor)
+    con = create_connection()
+    try:
+        with con:
+            cur = con.cursor(pymysql.cursors.DictCursor)
 
-        cur.execute("select * from watches where sku=%s", sku)
+            cur.execute("select * from watches where sku=%s", sku)
 
-        response = cur.fetchone()
+            response = cur.fetchone()
 
-        # If no watch found, return 404
-        if response is None:
-            abort(404)
+            # If no watch found, return 404
+            if response is None:
+                abort(404)
+    
+        return jsonify(response)
 
-    return jsonify(response)
-
+    finally:
+        con.close()
 
 @app.route(f'{api_info_v1_prefix}/watch/find')
 @auth.login_required
 def find_watch():
-
+    con = create_connection()
     find_params = {}
 
     find_params['sku'] = request.args.get('sku', None)
@@ -68,89 +72,109 @@ def find_watch():
     if find_params['sku'] is not None:
         find_params['sku'] += '%'
 
-    with con:
-        cur = con.cursor(pymysql.cursors.DictCursor)
+    try:
+        with con:
+            cur = con.cursor(pymysql.cursors.DictCursor)
 
-        sql = "select * from watches"
+            sql = "select * from watches"
 
-        first_where = True
+            first_where = True
 
-        for k, v in find_params.items():
-            if k == 'sku':
-                equals = 'like'
-            else:
-                equals = '='
-            if v is not None and first_where:
-                sql += f" where {k} {equals} %s"
-                first_where = False
-            elif v is not None and not first_where:
-                sql += f" and {k} {equals} %s"
+            for k, v in find_params.items():
+                if k == 'sku':
+                    equals = 'like'
+                else:
+                    equals = '='
+                if v is not None and first_where:
+                    sql += f" where {k} {equals} %s"
+                    first_where = False
+                elif v is not None and not first_where:
+                    sql += f" and {k} {equals} %s"
 
-        cur.execute(sql, [v for v in find_params.values() if v is not None])
+            cur.execute(sql, [v for v in find_params.values() if v is not None])
 
-        response = cur.fetchall()
+            response = cur.fetchall()
 
-        # If no watches found, return 404
-        if len(response) == 0:
-            abort(404)
+            # If no watches found, return 404
+            if len(response) == 0:
+                abort(404)
 
-    return jsonify(response)
+        return jsonify(response)
+
+    finally:
+        con.close()
 
 
 @app.route(f'{api_info_v1_prefix}/watch/complete-sku/<prefix>')
 @auth.login_required
 def complete_sku(prefix):
+    con = create_connection()
     prefix += '%'  # add wildcard for sql query
-    with con:
-        cur = con.cursor(pymysql.cursors.DictCursor)
+    try:
+        with con:
+            cur = con.cursor(pymysql.cursors.DictCursor)
 
-        cur.execute('select sku from watches where sku like %s', prefix)
+            cur.execute('select sku from watches where sku like %s', prefix)
 
-        response = cur.fetchall()
+            response = cur.fetchall()
 
-        # If no sku corresponds to the prefix, return 404
-        if len(response) == 0:
-            abort(404)
+            # If no sku corresponds to the prefix, return 404
+            if len(response) == 0:
+                abort(404)
 
-    return jsonify(response)
+        return jsonify(response)
+
+    finally:
+        con.close()
 
 
 @app.route(f'{api_info_v1_prefix}/watch', methods=['POST'])
 @auth.login_required
 def add_watch():
-    with con:
-        cur = con.cursor(pymysql.cursors.DictCursor)
-        sql = f'insert into watches ({", ".join([k for k in request.json.keys()])}) values ({", ".join(["%s" for v in request.json.values()])})'
-        try:
-            cur.execute(sql, [v for v in request.json.values()])
-        # Return 400 if e.g. an unexisting attribute has been specified, or if the sku already exists
-        except pymysql.Error:
-            abort(400)
+    con = create_connection()
+    try:
+        with con:
+            cur = con.cursor(pymysql.cursors.DictCursor)
+            sql = f'insert into watches ({", ".join([k for k in request.json.keys()])}) values ({", ".join(["%s" for v in request.json.values()])})'
+            try:
+                cur.execute(sql, [v for v in request.json.values()])
+            # Return 400 if e.g. an unexisting attribute has been specified, or if the sku already exists
+            except pymysql.Error:
+                abort(400)
 
-    return jsonify(success=True)
+        return jsonify(success=True)
+
+    finally:
+        con.close()
 
 
 @app.route(f'{api_info_v1_prefix}/watch/<sku>', methods=['DELETE'])
 @auth.login_required
 def delete_watch(sku):
-    with con:
-        cur = con.cursor(pymysql.cursors.DictCursor)
-        try:
-            cur.execute('delete from watches where sku = %s', sku)
+    con = create_connection()
+    try:
+        with con:
+            cur = con.cursor(pymysql.cursors.DictCursor)
+            try:
+                cur.execute('delete from watches where sku = %s', sku)
 
-            # If no rows has been affected by the delete, it means that the specified sku didn't exist, return 404
-            if cur.rowcount == 0:
-                abort(404)
-        # If something other went wrong, return 400
-        except pymysql.Error:
-            abort(400)
+                # If no rows has been affected by the delete, it means that the specified sku didn't exist, return 404
+                if cur.rowcount == 0:
+                    abort(404)
+            # If something other went wrong, return 400
+            except pymysql.Error:
+                abort(400)
 
-    return jsonify(success=True)
+        return jsonify(success=True)
+
+    finally:
+        con.close()
 
 
 @app.route(f'{api_info_v1_prefix}/watch/<sku>', methods=['PUT'])
 @auth.login_required
 def update_watch(sku):
+    con = create_connection()
     update_params = {}
 
     update_params['sku'] = request.get_json().get('sku', None)
@@ -165,27 +189,31 @@ def update_watch(sku):
     update_params['bracelet_material'] = request.get_json().get('bracelet_material', None)
     update_params['movement'] = request.get_json().get('movement', None)
 
+    try:
 
-    # Workaround: otherwise, it will return a 404 error if the new sku is equal to the old sku (a 400 error is more clear than a 404 error in this case)
-    if update_params['sku'] is not None and update_params['sku'] == sku:
-        abort(400)
-
-    with con:
-        cur = con.cursor(pymysql.cursors.DictCursor)
-
-        sql = 'update watches set ' + ', '.join([f'{k} = %s' for k, v in update_params.items() if v is not None]) + ' where sku = %s'
-        try:
-            cur.execute(sql, [v for v in update_params.values() if v is not None]+[sku])
-
-            # If no rows has been updated, it means that the specified sku doesn't exist, return 404
-            if cur.rowcount == 0:
-                abort(404)
-
-        # If invalid input, return 400
-        except pymysql.Error:
+        # Workaround: otherwise, it will return a 404 error if the new sku is equal to the old sku (a 400 error is more clear than a 404 error in this case)
+        if update_params['sku'] is not None and update_params['sku'] == sku:
             abort(400)
 
-    return jsonify(success=True)
+        with con:
+            cur = con.cursor(pymysql.cursors.DictCursor)
+
+            sql = 'update watches set ' + ', '.join([f'{k} = %s' for k, v in update_params.items() if v is not None]) + ' where sku = %s'
+            try:
+                cur.execute(sql, [v for v in update_params.values() if v is not None]+[sku])
+
+                # If no rows has been updated, it means that the specified sku doesn't exist, return 404
+                if cur.rowcount == 0:
+                    abort(404)
+
+            # If invalid input, return 400
+            except pymysql.Error:
+                abort(400)
+
+        return jsonify(success=True)
+        
+    finally:
+        con.close()
 
 
 @app.after_request
