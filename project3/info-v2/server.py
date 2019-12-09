b83dd96891eae1f55e0f755816f4c39633568fe3
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-12-09 22:13:52 +0100

    adapted server code to dynamodb

--- project3/info-v2/server.py
+++ project3/info-v2/server.py
@@ -5,23 +5,20 @@ from flask import Flask, abort, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
-import pymysql
-
-db_host = os.environ['DB_HOST']
-db_port = os.environ['DB_PORT']
-db_dbname = os.environ['DB_DBNAME']
-db_user = os.environ['DB_USER']
-db_pass = os.environ['DB_PASS']
+import boto3
 
 http_user = os.environ['HTTP_USER']
 http_pass = os.environ['HTTP_PASS']
 
-api_info_v1_prefix = '/info/v1'
-
-con = pymysql.connect(db_host, db_user, db_pass, db_dbname)
+api_info_v2_prefix = '/info/v2'
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+
+
+def get_connection():
+    # works if aws configure in aws cli has been used
+    return boto3.resource('dynamodb', region_name='eu-west-3')
 
 
 @auth.verify_password
@@ -31,38 +28,39 @@ def verify_password(username, password):
     return False
 
 
-@app.route(f'{api_info_v1_prefix}/watch/<sku>', methods=['GET'])
+@app.route(f'{api_info_v2_prefix}/watch/<sku>', methods=['GET'])
 @auth.login_required
 def get_watch_data(sku):
-    with con:
-        cur = con.cursor(pymysql.cursors.DictCursor)
+    connection = get_connection()
 
-        cur.execute("select * from watches where sku=%s", sku)
+    table = connection.Table('watches')
 
-        response = cur.fetchone()
+    try:
+        response = table.get_item(Key={'sku': sku})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        abort(404)
+    else:
+        item = response['Item']
+        item['year'] = int(item['year'])
+        return jsonify(item)
 
-        # If no watch found, return 404
-        if response is None:
-            abort(404)
 
-    return jsonify(response)
-
-
-
-@app.route(f'{api_info_v1_prefix}/watch', methods=['POST'])
+@app.route(f'{api_info_v2_prefix}/watch', methods=['POST'])
 @auth.login_required
 def add_watch():
-    with con:
-        cur = con.cursor(pymysql.cursors.DictCursor)
-        sql = f'insert into watches ({", ".join([k for k in request.json.keys()])}) values ({", ".join(["%s" for v in request.json.values()])})'
-        try:
-            cur.execute(sql, [v for v in request.json.values()])
-        # Return 400 if e.g. an unexisting attribute has been specified, or if the sku already exists
-        except pymysql.Error:
-            abort(400)
 
-    return jsonify(success=True)
+    connection = get_connection()
 
+    table = connection.Table('watches')
+
+    try:
+        response = table.put_item(Item=request.json)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        abort(404)
+    else:
+        return jsonify(success=True)
 
 @app.after_request
 def add_header(response):
@@ -72,9 +70,12 @@ def add_header(response):
     response.add_etag()
 
     # Headers for testing with swagger
-    response.headers.add('Access-Control-Allow-Origin', 'https://editor.swagger.io')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization')
-    response.headers.add('Access-Control-Allow-Methods','GET, POST, DELETE, PUT, PATCH, OPTIONS')
+    response.headers.add('Access-Control-Allow-Origin',
+                         'https://editor.swagger.io')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type, api_key, Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET, POST, DELETE, PUT, PATCH, OPTIONS')
     return response
 
 
