7d3ecd124591b3bc219626050686c94ba64cc0bc
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-12-10 16:42:32 +0100

    create-api-gateway WIP 3

--- project3/info-v2/create_api_gateway.sh
+++ project3/info-v2/create_api_gateway.sh
@@ -2,7 +2,12 @@
 aws apigateway import-rest-api --body 'file://info_openapi_v2.yaml' > json_output
 aws lambda list-functions > lambda_info
 
-aws apigateway put-integration --rest-api-id `python3 extract_api_gateway_id.py` --resource-id `python3 extract_lambda_id.py` --http-method GET --type AWS_PROXY --integration-http-method POST --uri `python3 extract_lambda_arn.py`
+aws apigateway get-resources --rest-api-id `python3 extract_api_gateway_id.py` > ressources_info
 
-rm json_output
-rm lambda_info
+aws apigateway put-integration --rest-api-id `python3 extract_api_gateway_id.py` --resource-id `python3 extract_get_ressource_info.py` --http-method GET --type AWS_PROXY --integration-http-method POST --uri 'arn:aws:apigateway:eu-west-3:lambda:path/2015-03-31/functions/`python3 extract_lambda_arn.py`'
+
+aws apigateway put-integration --rest-api-id `python3 extract_api_gateway_id.py` --resource-id `python3 extract_post_ressource_info.py` --http-method POST --type AWS_PROXY --integration-http-method POST --uri 'arn:aws:apigateway:eu-west-3:lambda:path/2015-03-31/functions/`python3 extract_lambda_arn.py`'
+
+#rm json_output
+#rm lambda_info
+#rm ressources_info
---
+++ project3/info-v2/extract_get_ressource_info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+if __name__=='__main__':
+    with open('./ressources_info') as f:
+        text = f.read()
+        json_output = json.loads(text)
+        for d in json_output['items']:
+            if d['path']=='/watch/{sku}':
+                print(d['id'])
