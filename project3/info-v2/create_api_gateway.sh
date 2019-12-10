c7d776d16b2379020581199ee9947aca4b2c729a
Naralas <ludovic.herbelin@gmail.com>
2019-12-10 17:53:48 +0100

    small changes on the create api gateway for clarity and just values for my AWS stuff

--- project3/info-v2/create_api_gateway.sh
+++ project3/info-v2/create_api_gateway.sh
@@ -4,9 +4,23 @@ aws lambda list-functions > lambda_info
 
 aws apigateway get-resources --rest-api-id `python3 extract_api_gateway_id.py` > ressources_info
 
-aws apigateway put-integration --rest-api-id `python3 extract_api_gateway_id.py` --resource-id `python3 extract_get_ressource_info.py` --http-method GET --type AWS_PROXY --integration-http-method POST --uri 'arn:aws:apigateway:eu-west-3:lambda:path/2015-03-31/functions/`python3 extract_lambda_arn.py`'
+echo "Uri : arn:aws:apigateway:eu-west-3:lambda:path/2015-03-31/functions/" $(python3 extract_lambda_arn.py)
 
-aws apigateway put-integration --rest-api-id `python3 extract_api_gateway_id.py` --resource-id `python3 extract_post_ressource_info.py` --http-method POST --type AWS_PROXY --integration-http-method POST --uri 'arn:aws:apigateway:eu-west-3:lambda:path/2015-03-31/functions/`python3 extract_lambda_arn.py`'
+aws apigateway put-integration \
+    --rest-api-id `python3 extract_api_gateway_id.py` \
+    --resource-id `python3 extract_get_ressource_info.py` \
+    --http-method GET \
+    --type AWS_PROXY \
+    --integration-http-method POST \
+    --uri 'arn:aws:apigateway:eu-west-3:lambda:path/2015-03-31/functions/`python3 extract_lambda_arn.py`' 
+
+aws apigateway put-integration \
+    --rest-api-id `python3 extract_api_gateway_id.py` \
+    --resource-id `python3 extract_post_ressource_info.py` \
+    --http-method POST \
+    --type AWS_PROXY \
+    --integration-http-method POST \
+    --uri 'arn:aws:apigateway:eu-west-3:lambda:path/2015-03-31/functions/`python3 extract_lambda_arn.py`'
 
 #rm json_output
 #rm lambda_info
